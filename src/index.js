392010d54c06f54098c4700b81e749aa22cd1c3a
Ross Dyson <dyson.ross@gmail.com>
2016-08-18 23:17:28 -0700

    2nd slider

--- src/index.js
+++ src/index.js
@@ -3,13 +3,29 @@ const slides = document.querySelectorAll('#slides.one .slide');
 var currSlide = 0;
 
 const slideInterval = () => {
-    setInterval(nextslide,2000);
+    setInterval(nextslide, 3000);
 }
 
 const nextslide = () => {
     slides[currSlide].className = 'slide';
-    currSlide = (currSlide+1)%slides.length;
+    currSlide = (currSlide + 1) % slides.length;
     slides[currSlide].className = 'slide showing';
 }
 
 slideInterval();
+
+const slidesTwo = document.querySelectorAll('#slides.two .slide');
+
+var currSlideTwo = 0;
+
+const slideIntervalTwo = () => {
+    setInterval(nextslideTwo, 3000);
+}
+
+const nextslideTwo = () => {
+    slidesTwo[currSlideTwo].className = 'slide slide-out';
+    currSlideTwo = (currSlideTwo + 1) % slidesTwo.length;
+    slidesTwo[currSlideTwo].className = 'slide slide-in';
+}
+
+slideIntervalTwo();
